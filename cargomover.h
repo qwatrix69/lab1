39e2e657e157daab27faff967036982b71a38f1c
Aleksandar Kostyukevich <aleksandar.kostyukevich@yandex.ru>
2024-09-08 16:31:42 +0300

    name

--- cargomover.h
+++ cargomover.h
@@ -3,15 +3,15 @@
 
 using namespace std;
 
-class Base
+class Cargomover
 {
 private:
     string name;
 public:
-    Base();
-    Base(const string& n);
-    Base(const Base& other);
-    virtual ~Base();
+    Cargomover();
+    Cargomover(const string& n);
+    Cargomover(const Cargomover& other);
+    virtual ~Cargomover();
 
     virtual void set_value(const string& v) = 0;
     virtual void get_value() = 0;
