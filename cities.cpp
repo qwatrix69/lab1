62fb413b85a5427a32850d4957dfb62f4e996d26
Aleksandar Kostyukevich <aleksandar.kostyukevich@yandex.ru>
2024-09-11 16:17:08 +0300

    only exceptions and checks remaining

--- cities.cpp
+++ cities.cpp
@@ -3,22 +3,22 @@
 Cities::Cities() : cities(nullptr), size(0), capacity(0) {}
 
 Cities::Cities(int& c) : size(c), capacity(c) {
-    cities = (char**)calloc(c, sizeof(char*));
-        if (!cities) {
-            cerr << "Ошибка выделения памяти!" << endl;
-            exit(1);
-        }
+    cities = new char*[c];
+    if (!cities) {
+        cerr << "Ошибка выделения памяти!" << endl;
+        exit(1);
+    }
 }
 
 Cities::Cities(const Cities& other) : size(other.size), capacity(other.capacity) {
-    cities = (char**)calloc(capacity, sizeof(char*));
+    cities = new char*[capacity];
     if (!cities) {
         cerr << "Ошибка выделения памяти при копировании!" << endl;
         exit(1);
     }
 
     for (int i = 0; i < size; ++i) {
-        cities[i] = (char*)malloc((strlen(other.cities[i]) + 1) * sizeof(char));
+        cities[i] = new char[strlen(other.cities[i]) + 1];
         if (!cities[i]) {
             cerr << "Ошибка выделения памяти для города!" << endl;
             exit(1);
@@ -33,11 +33,17 @@ Cities::~Cities() {
 
 void Cities::resize() {
     int new_capacity = (capacity == 0) ? 1 : capacity * 2;
-    char** new_cities = (char**)realloc(cities, new_capacity * sizeof(char*));
+    char** new_cities = new char*[new_capacity];
     if (!new_cities) {
         cerr << "Ошибка выделения памяти при изменении размера!" << endl;
         exit(1);
     }
+
+    for (int i = 0; i < size; ++i) {
+        new_cities[i] = cities[i];
+    }
+
+    delete[] cities;
 
     cities = new_cities;
     capacity = new_capacity;
@@ -52,14 +58,14 @@ Cities& Cities::operator=(const Cities& other) {
 
     size = other.size;
     capacity = other.capacity;
-    cities = (char**)calloc(capacity, sizeof(char*));
+    cities = new char*[capacity];
     if (!cities) {
         cerr << "Ошибка выделения памяти!" << endl;
         exit(1);
     }
 
     for (int i = 0; i < size; ++i) {
-        cities[i] = (char*)malloc((strlen(other.cities[i]) + 1) * sizeof(char));
+        cities[i] = new char[strlen(other.cities[i]) + 1];
         if (!cities[i]) {
             cerr << "Ошибка выделения памяти для города!" << endl;
             exit(1);
@@ -75,7 +81,7 @@ void Cities::add_city(const char* c) {
         resize();
     }
 
-    cities[size] = (char*)malloc((strlen(c) + 1) * sizeof(char));
+    cities[size] = new char[strlen(c) + 1];
     if (!cities[size]) {
         cerr << "Ошибка выделения памяти для нового города!" << endl;
         exit(1);
@@ -86,17 +92,16 @@ void Cities::add_city(const char* c) {
 
 void Cities::display_cities() {
     for (int i = 0; i < size; ++i) {
-            cout << (i + 1) << ". " << cities[i] << endl;
-        }
+        cout << (i + 1) << ". " << cities[i] << endl;
+    }
 }
-
 
 void Cities::clear_cities() {
     if (cities) {
         for (int i = 0; i < size; ++i) {
-            free(cities[i]);
+            delete[] cities[i];
         }
-        free(cities);
+        delete[] cities;
         cities = nullptr;
     }
     size = 0;
@@ -109,7 +114,7 @@ void Cities::input_cities() {
     int num_cities;
     cout << "Введите количество городов: ";
     cin >> num_cities;
-    cin.ignore();  // Очистка буфера ввода
+    cin.ignore();
 
     for (int i = 0; i < num_cities; ++i) {
         string city;
